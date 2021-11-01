289b6c13678dceb3f5db58c4cdb8e6638eff609e
Aayush Mittal <aayush.mittal@blackstone.com>
2021-11-01 13:51:50 +0530

    tdd

---
+++ TDD/StringCalculator.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDD
+{
+    public class StringCalculator
+    {
+        public int Calculate(string input)
+        {
+            var delimiters = new List<string> { ",", "\n" };
+            if (string.IsNullOrEmpty(input.Trim()))
+            {
+                return 0;
+            }
+
+            if (input.StartsWith("//"))
+            {
+                if (input.StartsWith("//["))
+                {
+                    delimiters.AddRange(input.Split("\n").First().Split(new char[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries));
+                }
+                else
+                {
+                    delimiters.Add(input.Substring(2, 1));
+                }
+                input = input.Split("\n").Last();
+            }
+
+            var numbers = input.Split(delimiters.ToArray(), StringSplitOptions.None).Select(x => int.Parse(x));
+
+            var negativeNumbers = numbers.Where(x => x < 0);
+            if (negativeNumbers.Any())
+            {
+                throw new Exception("Negatives Not Allowed" + string.Join(',', negativeNumbers));
+            }
+
+            return numbers.Where(x => x < 1000).Sum();
+        }
+    }
+}
---
+++ TDD/StringCalculatorTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TDD
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1,2", 3)]
+        public void AddTwoNumbersTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [TestCase("", 0)]
+        [TestCase("1", 1)]
+        [TestCase("1,2,3", 6)]
+        public void AddUnknownAmoutNumbersTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [TestCase("1\n2,3", 6)]
+        public void AddUnknownAmoutNumbersWithDiffDelimitersTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+
+        [TestCase("//;\n1;2", 3)]
+        public void AddUnknownAmoutNumbersWithDiffDefaultDelimitersTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+        }
+
+        [TestCase("1, 2, -1", "-1")]
+        public void NegativeNotAllowedTest(string input, string output)
+        {
+            var calculator = new StringCalculator();
+
+
+            Assert.That(() => calculator.Calculate(input), Throws.TypeOf<Exception>().With.Message.EqualTo("Negatives Not Allowed" + output));
+        }
+
+        [TestCase("2,1001", 2)]
+        public void NumberGreaterThanThousandTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+
+        }
+
+        [TestCase("//[***]\n1***2***3", 6)]
+        public void AnyLengthDelimiterTest(string input, int output)
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+
+        }
+
+        [TestCase("//[A][b]\n1A2b3", 6)]
+        public void add_multipledelimiters_allused(string input, int output)
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Calculate(input);
+
+            Assert.AreEqual(output, result);
+
+        }
+    }
+}
